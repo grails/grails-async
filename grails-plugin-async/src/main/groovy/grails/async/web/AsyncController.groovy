11788888c278e6dd464b1a321a8a11ba2361e960
graemerocher <graeme.rocher@gmail.com>
2017-04-03 15:33:16 +0200

    Update documentation / Deprecate AsyncController and provide replacement

--- grails-plugin-async/src/main/groovy/grails/async/web/AsyncController.groovy
+++ grails-plugin-async/src/main/groovy/grails/async/web/AsyncController.groovy
@@ -1,24 +1,9 @@
-/*
- * Copyright 2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package grails.artefact
+package grails.async.web
 
-import grails.async.web.AsyncGrailsWebRequest
-import grails.events.Events
+import org.grails.plugins.web.async.GrailsAsyncContext
+import org.grails.web.servlet.mvc.GrailsWebRequest
 import org.grails.web.util.GrailsApplicationAttributes
-import groovy.transform.CompileStatic
+import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.async.AsyncWebRequest
 import org.springframework.web.context.request.async.WebAsyncManager
 import org.springframework.web.context.request.async.WebAsyncUtils
@@ -26,20 +11,13 @@ import org.springframework.web.context.request.async.WebAsyncUtils
 import javax.servlet.AsyncContext
 import javax.servlet.http.HttpServletRequest
 
-import org.grails.plugins.web.async.GrailsAsyncContext
-import org.grails.web.servlet.mvc.GrailsWebRequest
-import org.springframework.web.context.request.RequestContextHolder
-
 /**
- * 
- * @author Jeff Brown
- * @since 3.0
- * @deprecated Use {@link grails.async.web.AsyncController} instead
+ * Exposes a startAsync() method for access to the Servlet 3.x API
+ *
+ * @author Graeme Rocher
+ * @since 3.3
  */
-@CompileStatic
-@Deprecated
-trait AsyncController extends Events {
-
+trait AsyncController {
     /**
      * Raw access to the Servlet 3.0 startAsync method
      *
