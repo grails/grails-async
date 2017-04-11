bd60d74a61867160aa9028e327b22104d90cfa49
graemerocher <graeme.rocher@gmail.com>
2017-04-11 08:32:01 +0200

    Restore publishing state

--- travis-build.sh
+++ travis-build.sh
@@ -33,30 +33,30 @@ if [[ -n $TRAVIS_TAG ]] || [[ $TRAVIS_BRANCH =~ ^master$ && $TRAVIS_PULL_REQUEST
   fi
 
   # If there is a tag present then this becomes the latest
-#  if [[ -n $TRAVIS_TAG ]]; then
-#      mkdir -p latest
-#      cp -r ../build/docs/. ./latest/
-#      git add latest/*
-#
-#      version="$TRAVIS_TAG"
-#      version=${version:1}
-#      majorVersion=${version:0:4}
-#      majorVersion="${majorVersion}x"
-#
-#      mkdir -p "$version"
-#      cp -r ../build/docs/. "./$version/"
-#      git add "$version/*"
-#
-#      mkdir -p "$majorVersion"
-#      cp -r ../build/docs/. "./$majorVersion/"
-#      git add "$majorVersion/*"
-#
-#  fi
-#
-#  git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
-#  git push origin HEAD
-#  cd ..
-#  rm -rf gh-pages
+  if [[ -n $TRAVIS_TAG ]]; then
+      mkdir -p latest
+      cp -r ../build/docs/. ./latest/
+      git add latest/*
+
+      version="$TRAVIS_TAG"
+      version=${version:1}
+      majorVersion=${version:0:4}
+      majorVersion="${majorVersion}x"
+
+      mkdir -p "$version"
+      cp -r ../build/docs/. "./$version/"
+      git add "$version/*"
+
+      mkdir -p "$majorVersion"
+      cp -r ../build/docs/. "./$majorVersion/"
+      git add "$majorVersion/*"
+
+  fi
+
+  git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+  git push origin HEAD
+  cd ..
+  rm -rf gh-pages
 fi
 
 exit $EXIT_STATUS
