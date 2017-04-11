518dd10be2f0fdba13a17032c8a10a49dcac14fc
graemerocher <graeme.rocher@gmail.com>
2017-04-11 08:46:04 +0200

    Documentation updates

--- examples/pubsub-demo/src/main/groovy/pubsub/demo/BookSubscriber.groovy
+++ examples/pubsub-demo/src/main/groovy/pubsub/demo/BookSubscriber.groovy
@@ -4,19 +4,23 @@ import grails.events.annotation.Subscriber
 import org.grails.datastore.mapping.engine.event.PreInsertEvent
 import org.springframework.stereotype.Component
 
+import java.util.concurrent.ConcurrentLinkedDeque
+
 @Component
 class BookSubscriber {
 
     List<String> newBooks =[]
-    List<PreInsertEvent> insertEvents =[]
-
     @Subscriber("newBook")
     void withBook(Book book) {
         newBooks.add(book.title)
     }
 
+    // tag::gorm[]
+    Collection<PreInsertEvent> insertEvents = new ConcurrentLinkedDeque<>()
+
     @Subscriber
     void beforeInsert(PreInsertEvent event) {
         insertEvents.add(event)
     }
+    // end::gorm[]
 }
