f920902a25aaeb027eb96c684ad46adfbe1dc2bb
Joe McCall <joemccall@intrepid-networks.com>
2022-03-23 08:25:27 -0400

    Ensure reply closure is called on event subscriptions
    * Follow the same pattern in place for Closure subscriptions
    * Call the reply closure if it exists on both failure and success cases
    * Ensure backwards-compatibility by setting the reply closure parameter to null

--- grails-events/src/main/groovy/org/grails/events/EventSubscriberTrigger.groovy
+++ grails-events/src/main/groovy/org/grails/events/EventSubscriberTrigger.groovy
@@ -18,24 +18,36 @@ import groovy.util.logging.Slf4j
 class EventSubscriberTrigger implements EventTrigger {
     final Event event
     final Subscriber subscriber
+    final Closure reply
 
-    EventSubscriberTrigger(Event event, Subscriber subscriber) {
+    EventSubscriberTrigger(Event event, Subscriber subscriber, Closure reply = null) {
         this.event = event
         this.subscriber = subscriber
+        this.reply = reply
     }
 
     @Override
     Object proceed() {
         try {
+            def result
             if(subscriber instanceof EventSubscriber) {
-                return subscriber.call(event)
+                result = subscriber.call(event)
             }
             else {
-                return subscriber.call(event.data)
+                result = subscriber.call(event.data)
             }
+            if(reply != null) {
+                return reply.call(result)
+            }
+            return result
         } catch (Throwable e) {
-            log.error("Error triggering event [$event.id] for subscriber [${subscriber}]: $e.message", e)
-            throw e
+            if(reply != null && reply.parameterTypes && reply.parameterTypes[0].isInstance(e)) {
+                reply.call(e)
+            }
+            else {
+                log.error("Error triggering event [$event.id] for subscriber [${subscriber}]: $e.message", e)
+                throw e
+            }
         }
     }
 }
